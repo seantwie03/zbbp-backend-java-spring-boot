da78c8d08ea4a581c6b1f283235c80bb7a32d0c6
Sean Twie03 <sean.twie03@gmail.com>
2022-10-06 20:40:32 -0500

    Fix formatting

--- src/test/java/me/seantwiehaus/zbbp/domain/MoneyTest.java
+++ src/test/java/me/seantwiehaus/zbbp/domain/MoneyTest.java
@@ -6,29 +6,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoneyTest {
-    private static Money money1;
-    private static Money money2;
+  private static Money money1;
+  private static Money money2;
 
-    @BeforeAll
-    static void setup() {
-        money1 = new Money(25.50);
-        money2 = new Money(2550);
-    }
+  @BeforeAll
+  static void setup() {
+    money1 = new Money(25.50);
+    money2 = new Money(2550);
+  }
 
-    @Test
-    void shouldReturnCorrectValueInCents() {
-        assertEquals(2550, money1.inCents());
-        assertEquals(2550, money2.inCents());
-    }
+  @Test
+  void shouldReturnCorrectValueInCents() {
+    assertEquals(2550, money1.inCents());
+    assertEquals(2550, money2.inCents());
+  }
 
-    @Test
-    void shouldReturnCorrectValueInDollars() {
-        assertEquals(25.50, money1.inDollars());
-        assertEquals(25.50, money2.inDollars());
-    }
+  @Test
+  void shouldReturnCorrectValueInDollars() {
+    assertEquals(25.50, money1.inDollars());
+    assertEquals(25.50, money2.inDollars());
+  }
 
-    @Test
-    void shouldHaveEqualityWhenCreatedWithDifferentConstructors() {
-        assertEquals(money1, money2);
-    }
+  @Test
+  void shouldHaveEqualityWhenCreatedWithDifferentConstructors() {
+    assertEquals(money1, money2);
+  }
 }
