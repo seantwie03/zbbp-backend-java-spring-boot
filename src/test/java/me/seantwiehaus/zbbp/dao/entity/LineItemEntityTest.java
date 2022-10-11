cd59f71f7d51f3912f40d877a0fbbe39ae84c8b9
Sean Twie03 <sean.twie03@gmail.com>
2022-10-11 05:51:18 -0500

    Remove worthless test

--- src/test/java/me/seantwiehaus/zbbp/dao/entity/LineItemEntityTest.java
+++ src/test/java/me/seantwiehaus/zbbp/dao/entity/LineItemEntityTest.java
@@ -3,8 +3,6 @@ package me.seantwiehaus.zbbp.dao.entity;
 import me.seantwiehaus.zbbp.domain.ItemType;
 import org.junit.jupiter.api.Test;
 
-import java.time.YearMonth;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LineItemEntityTest {
@@ -16,15 +14,4 @@ class LineItemEntityTest {
     // Then the type should be Expense
     assertEquals(ItemType.EXPENSE, lineItem.getType());
   }
-
-  @Test
-  void budgetDateShouldAlwaysBeTheFirstDayOfTheMonth() {
-    // Given a LineItemEntity
-    LineItemEntity lineItem = new LineItemEntity();
-    // When setBudgetDate is called with any day of the month
-    lineItem.setBudgetDate(YearMonth.of(2030, 1));
-    // Then the budgetDate should be set to the first day of the same month
-    assertEquals(YearMonth.of(2030, 1), lineItem.getBudgetDate());
-  }
-
 }
