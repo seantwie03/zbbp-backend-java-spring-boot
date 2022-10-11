59533ed163344190edf268c64fd38ee6d0e60f93
Sean Twie03 <sean.twie03@gmail.com>
2022-10-11 05:23:42 -0500

    Add delete tests

--- src/test/java/me/seantwiehaus/zbbp/dao/repository/LineItemRepositoryIT.java
+++ src/test/java/me/seantwiehaus/zbbp/dao/repository/LineItemRepositoryIT.java
@@ -1,6 +1,7 @@
 package me.seantwiehaus.zbbp.dao.repository;
 
 import me.seantwiehaus.zbbp.dao.entity.LineItemEntity;
+import me.seantwiehaus.zbbp.dao.entity.TransactionEntity;
 import me.seantwiehaus.zbbp.domain.Category;
 import me.seantwiehaus.zbbp.domain.ItemType;
 import org.junit.jupiter.api.Nested;
@@ -9,22 +10,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class LineItemRepositoryIT {
   @Autowired
   TestEntityManager entityManager;
   @Autowired
-  LineItemRepository lineItemRepository;
+  LineItemRepository repository;
 
   @Nested
-  class FindTopByOrderByBudgetDateDesc {
+  class WhenFindTopByOrderByBudgetDateDesc {
     @Test
     void shouldReturnMostRecentLineItem() {
       // Given two LineItems with different dates
@@ -34,7 +35,7 @@ class LineItemRepositoryIT {
       entityManager.persistAndFlush(shouldNot);
       entityManager.clear(); // Clear the context so that entities are not fetched from the first-level cache
       // When findTopByOrderByBudgetDateDesc is called
-      Optional<LineItemEntity> mostRecent = lineItemRepository.findTopByOrderByBudgetDateDesc();
+      Optional<LineItemEntity> mostRecent = repository.findTopByOrderByBudgetDateDesc();
       // Then the most recent LineItemEntity should be returned
       assertTrue(mostRecent.isPresent());
       assertEquals(shouldBeTop, mostRecent.get());
@@ -43,7 +44,7 @@ class LineItemRepositoryIT {
   }
 
   @Nested
-  class FindAllByBudgetDateBetween {
+  class WhenFindAllByBudgetDateBetween {
     @Test
     void shouldReturnLineItemsBetweenTwoDates() {
       // Given three LineItems with different dates
@@ -56,13 +57,54 @@ class LineItemRepositoryIT {
       entityManager.clear(); // Clear the context so that entities are not fetched from the first-level cache
       // When findAllByBudgetDateBetween is called
       List<LineItemEntity> allBetween =
-          lineItemRepository.findAllByBudgetDateBetween(YearMonth.of(2022, 1), YearMonth.of(2022, 2));
+          repository.findAllByBudgetDateBetween(YearMonth.of(2022, 1), YearMonth.of(2022, 2));
       // Then only one LineItem should be returned
       assertEquals(2, allBetween.size());
       // And the LineItems for 2022,1 and 2022,2 should be included in the list
       assertTrue(allBetween.stream().allMatch(item -> item.equals(shouldBeIncluded) || item.equals(shouldAlsoBeIncluded)));
       // And the LineItems for 2022,3 should NOT
       assertTrue(allBetween.stream().noneMatch(item -> item.equals(shouldNotBeIncluded)));
+    }
+  }
+
+  @Nested
+  class WhenDelete {
+    @Test
+    void shouldDeleteLineItemWithNoTransactions() {
+      // Given a LineItem with no Transactions
+      LineItemEntity toDelete = createLineItemEntity(YearMonth.now());
+      entityManager.persistAndFlush(toDelete);
+      entityManager.clear();
+      // When the repository delete method is called
+      repository.delete(toDelete);
+      // Then the item should be deleted
+      Optional<LineItemEntity> shouldBeDeleted = repository.findLineItemEntityById(toDelete.getId());
+      assertTrue(shouldBeDeleted.isEmpty());
+    }
+
+    @Test
+    void shouldDeleteLineItemWithTwoTransactionsAndTransactionsShouldRemain() {
+      // Given a LineItem with two Transactions
+      LineItemEntity toDelete = createLineItemEntity(YearMonth.now());
+      entityManager.persistAndFlush(toDelete); // Persist and flush to get ID
+      TransactionEntity transaction1 = createTransactionEntity(LocalDate.now(), 5000, toDelete.getId());
+      entityManager.persist(transaction1);
+      TransactionEntity transaction2 = createTransactionEntity(LocalDate.now(), 6000, toDelete.getId());
+      entityManager.persist(transaction2);
+      toDelete.setTransactionEntities(List.of(transaction1, transaction2));
+      entityManager.persist(toDelete);
+      entityManager.flush();
+      entityManager.clear();
+      // When the repository delete method is called
+      repository.delete(toDelete);
+      // Then the item should be deleted
+      Optional<LineItemEntity> shouldBeDeleted = repository.findLineItemEntityById(toDelete.getId());
+      assertTrue(shouldBeDeleted.isEmpty());
+      // And the Transactions should still exist in the DB
+      TransactionEntity shouldExist1 = entityManager.find(TransactionEntity.class, transaction1.getId());
+      assertNotNull(shouldExist1);
+      TransactionEntity shouldExist2 = entityManager.find(TransactionEntity.class, transaction2.getId());
+      assertNotNull(shouldExist2);
     }
   }
 
@@ -74,5 +116,14 @@ class LineItemRepositoryIT {
     lineItemEntity.setPlannedAmount(120000);
     lineItemEntity.setCategory(Category.FOOD);
     return lineItemEntity;
+  }
+
+  private TransactionEntity createTransactionEntity(LocalDate date, int amount, Long lineItemId) {
+    TransactionEntity transactionEntity = new TransactionEntity();
+    transactionEntity.setDate(date);
+    transactionEntity.setMerchant("Merchant");
+    transactionEntity.setAmount(amount);
+    transactionEntity.setLineItemId(lineItemId);
+    return transactionEntity;
   }
 }
