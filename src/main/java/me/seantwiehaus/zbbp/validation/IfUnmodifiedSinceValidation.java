3784dbf2abea3426e77f860dc21f78682f4826ea
Sean Twie03 <sean.twie03@gmail.com>
2022-10-17 05:43:56 -0500

    Add null checks

--- src/main/java/me/seantwiehaus/zbbp/validation/IfUnmodifiedSinceValidation.java
+++ src/main/java/me/seantwiehaus/zbbp/validation/IfUnmodifiedSinceValidation.java
@@ -4,6 +4,7 @@ import me.seantwiehaus.zbbp.dao.entity.BaseEntity;
 import me.seantwiehaus.zbbp.exception.ResourceConflictException;
 
 import java.time.Instant;
+import java.util.Objects;
 
 public class IfUnmodifiedSinceValidation {
   private IfUnmodifiedSinceValidation() {
@@ -11,9 +12,15 @@ public class IfUnmodifiedSinceValidation {
 
   public static <T extends BaseEntity> void throwWhenEntityLastModifiedAtIsAfterIfUnmodifiedSince(
       T entity, Instant ifUnmodifiedSince) {
+    // Null checks because this is a utility method that may be used in a variety of contexts
+    Objects.requireNonNull(entity, "entity must not be null");
+    Objects.requireNonNull(
+        entity.getLastModifiedAt(),
+        "%s.lastModifiedAt must not be null".formatted(entity.getClass().getSimpleName()));
+    Objects.requireNonNull(ifUnmodifiedSince, "ifUnmodifiedSince must not be null");
     if (entity.getLastModifiedAt().isAfter(ifUnmodifiedSince)) {
       throw new ResourceConflictException(
-          "%s with ID=%d was modified after the provided If-Unmodified-Since header value of %s"
+          "%s with ID %d was modified after the provided If-Unmodified-Since header value of %s"
               .formatted(entity.getClass().getSimpleName(), entity.getId(), ifUnmodifiedSince));
     }
   }
