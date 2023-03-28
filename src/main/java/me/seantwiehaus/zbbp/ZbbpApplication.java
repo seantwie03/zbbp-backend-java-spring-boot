bb38b002ca5dd855020a498cde06d85d4464d941
= <=>
2023-03-28 05:57:05 -0500

    feat: add permissive CORS mapping

--- src/main/java/me/seantwiehaus/zbbp/ZbbpApplication.java
+++ src/main/java/me/seantwiehaus/zbbp/ZbbpApplication.java
@@ -1,12 +1,28 @@
 package me.seantwiehaus.zbbp;
 
+import lombok.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ZbbpApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ZbbpApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+      }
+    };
   }
 }
